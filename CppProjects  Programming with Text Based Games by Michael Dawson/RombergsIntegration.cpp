90566d954f0a8433f77f5e2bf8f53ce79cad55f1
Eldan Rey Dofredo <38562111+eldanrey@users.noreply.github.com>
2021-09-30 19:20:32 +0800

    Romberg Integration

---
+++ CppProjects  Programming with Text Based Games by Michael Dawson/RombergsIntegration.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int segments,upperBound,lowerBound;
+  cout<<"\t\tTHIS PROGRAM IS A CALCULATOR APP THAT CAN SOLVE INTEGRAL USING ROMBERG'S METHOD\n\n";
+  cout<<"\n[a1]X^2\t\t[a2]X^a\t\t[a3]Pi\n[a4]sqrt\t[a5]nth root\t[a6]e^x\n[a7]ln(x)\t[a8]log10(x)\t[a9]logx\n[a10]sin(x)\t[a11]cos(x)\t[a12]tan(x)\n[a13]sinh(x)\t[a14]cosh(x)\t[a15]tanh(x)\n";
+  cout<<"Enter the number of segments (n): ";
+  cin>>segments;
+  cout<<"Enter the Upper Bound: ";
+  cin>>upperBound;
+  cout<<"Enter the Lower Bound: ";
+  cin>>lowerBound;
+
+  
+}

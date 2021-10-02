011b743cf6612cfde7b4d229df19963aba8c30b5
Eldan Rey Dofredo <38562111+eldanrey@users.noreply.github.com>
2021-10-02 11:09:44 +0800

    Chapter 3 Project 4

--- CppProjects  Programming with Text Based Games by Michael Dawson/Ch3P4.cpp
+++ CppProjects  Programming with Text Based Games by Michael Dawson/Ch3P4.cpp
@@ -23,6 +23,47 @@ adjusted accordingly).
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    vector <vector<int>> highscore={{"Eldan",1000},{"Red",1000},{"Rey",1000},{"Elen",1000},{"Jane",1000}};
+    vector <vector<string>> highScore={{"Eldan","1000"},{"Red","1000"},{"Rey","1000"},{"Elen","1000"},{"Jane","1000"}};
+    cout<<"\t\tHIGSCHORE SHOW\n\n";
+for(int i=0;i<highScore.size();i++){
+        cout<<highScore[i][0]<<'\t'<<highScore[i][1]<<'\n';}
+while(1)
+    {
     
+    vector<string>temp;
+    string name;
+    int score;
+    cout<<"Enter a Name: ";
+    cin>>name;
+    cout<<"Enter a Score: ";
+    cin>>score;
+    for(int i=0;i<highScore.size();i++){
+        int currHighScore=stoi(highScore[i][1]);
+        if(score>=currHighScore){
+            currHighScore=score;
+            string stringScore=to_string(currHighScore);
+            temp.push_back(name);
+            temp.push_back(stringScore);
+            highScore.push_back(temp);
+             temp.clear();
+            break;
+        }
+    }
+    for(int i=0;i<highScore.size();i++){
+        for(int j=0;j<highScore.size()-1;j++){
+            int currScore=stoi(highScore[j][1]);
+            int forwardScore=stoi(highScore[j+1][1]);
+            if(currScore<forwardScore){
+               swap(highScore[j][1],highScore[j+1][1]);
+               swap(highScore[j][0],highScore[j+1][0]); 
+               
+            }
+        }
+    }
+    highScore.pop_back();
+    for(int i=0;i<highScore.size();i++){
+        cout<<"\n\n"<<highScore[i][0]<<'\t'<<highScore[i][1]<<'\n';
+    }}
+    
+
 }

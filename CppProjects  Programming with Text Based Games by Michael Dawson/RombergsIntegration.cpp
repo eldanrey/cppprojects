aee082d998947d5945baef4fa7671d27bb3b7962
Eldan Rey Dofredo <38562111+eldanrey@users.noreply.github.com>
2021-10-04 23:55:11 +0800

    Update RombergsIntegration.cpp

--- CppProjects  Programming with Text Based Games by Michael Dawson/RombergsIntegration.cpp
+++ CppProjects  Programming with Text Based Games by Michael Dawson/RombergsIntegration.cpp
@@ -12,20 +12,23 @@ int main(){
     int segments;
     string upperBound,lowerBound;
   cout<<"\t\tTHIS PROGRAM IS A CALCULATOR APP THAT CAN SOLVE INTEGRAL USING ROMBERG'S METHOD\n\n";
-  // cout<<"\n[a1]X^2\t\t[a2]X^a\t\t[a3]Pi\n[a4]sqrt\t[a5]nth root\t[a6]e^x\n[a7]ln(x)\t[a8]log10(x)\t[a9]logx\n[a10]sin(x)\t[a11]cos(x)\t[a12]tan(x)\n[a13]sinh(x)\t[a14]cosh(x)\t[a15]tanh(x)\n";
+  
   cout<<"\nEnter the number of segments (n): ";
   cin>>segments;
   cin.clear(); cin.sync();
   cout<<"\nEnter the Upper Bound: ";
   getline(cin,upperBound);
   cin.clear(); cin.sync();
-  cout<<"\nEnter the Lower Bound: ";
+  cout<<"\nEnter the Lower Bound: "; 
   getline(cin,lowerBound);
   cin.clear(); cin.sync();
   cout<<"\nEnter f(x): ";
   getline(cin,fx);
   cin.clear(); cin.sync();
   cout<<segments<<"\t"<<upperBound<<"\t"<<lowerBound<<"\t"<<fx<<"\t";
+  cout<<"\n[a1]X^2\t\t[a2]X^a\t\t[a3]Pi\n[a4]sqrt\t[a5]nth root\t[a6]e^x\n[a7]ln(x)\t[a8]log10(x)\t[a9]logx\n[a10]sin(x)\t[a11]cos(x)\t[a12]tan(x)\n[a13]sinh(x)\t[a14]cosh(x)\t[a15]tanh(x)\n";
+  cout<<"\nEnter your choice: ";
+  string choice;
   
  
 }

cf98485f716284219635a893d5079867cb9c577c
Eldan Rey Dofredo <38562111+eldanrey@users.noreply.github.com>
2021-10-26 21:07:44 +0800

    Added Factorial (recursion)

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int factorial(int value); 
+
+//5! = 1 * 2 * 3 * 4 * 5
+//   = 120
+     
+void content()
+{
+	int number;
+	cout << "This is an example program.\n";
+	cout << "A Programmer defined function within another function...\n";
+	cout << "Looping Program\n";
+	cout << "Factorial of a Number \n";
+	cout << "Enter an integer value : ";
+	cin >> number;
+	cout << factorial(number);
+}
+
+
+
+int factorial(int value)
+{
+	if (value==1)return 1;
+	return answer*(answer-1);
+}
+int main(){
+    content();
+}

99169d7e28ed1a53dc8831bcd959809f2d257ef9
Eldan Rey Dofredo <38562111+eldanrey@users.noreply.github.com>
2022-07-26 17:56:29 +0800

    just nothing

--- LoginRegister/LoginRegister.cpp
+++ LoginRegister/LoginRegister.cpp
@@ -1,15 +1,32 @@
 // LoginRegister.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+/*
+    What I Need To Do:
+        1.) Set up a class for Student's Info
+        2.) Read File from .dat to confirm the input idnumber and password
+
+
+*/
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+
 using std::string;
 using std::cout;
 using std::cin;
+std::ifstream readFile;
+std::ofstream writeFile;
+
 
 class Student {
+    
     int Age;
     string IdNumber;
     string Name;
+    string Password;
     int BirthDay, BirthMonth, BirthYear;
 
 public:
@@ -24,10 +41,53 @@ public:
         BirthMonth = birthmonth;
         BirthYear = birthyear;
     }
+    string getIdNumber() {
+        return IdNumber;
+    }
+    string getPassword() {
+        return Password;
+    }
+    int getAge() {
+        return Age;
+    }
+    string getName() {
+        return Name;
+    }
+    string getBirthDate() {
+            std::stringstream s;
+            s << BirthMonth << "/" << BirthDay << "/" << BirthYear;
+            string bd = s.str();
+        return bd;
+    }
 };
+
+
+bool compareCredentials(string idnumber, string password) {
+    readFile.open("records.dat", std::ios::in);
+    string ss;
+    std::stringstream s;
+
+}
+bool LoginSystem() {
+    string idnumber, password;
+    cout << "Id Number: ";
+    cin >> idnumber;
+    cout << "Password: ";
+    cin >> password;
+    if (compareCredentials(idnumber,password))return 1;
+    else return 0;
+}
+void Menu() {
+    cout << "\t\tWelcome To My Simple Login Register Application\n\n";
+
+}
 int main()
 {   
-    Student student1 = Student("Eldan", "S512938", 21, 10, 30, 2000);
+    if (LoginSystem())Menu();
+    cout << "WHAT";
+    
+    Student student1 = Student("Eldan", "S512938", 21, 11, 30, 2000);
+    cout << student1.getBirthDate();
     student1.status();
     std::cout << "Hello World!\n";
 
